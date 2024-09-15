7465b9b0ee36823ed3b0adf42b65aac668c909a9
Yas1th005 <yashpanatala@gmail.com>
2024-09-15 11:41:20 +0530

    Final

---
+++ http-server/index.js
@@ -0,0 +1,38 @@
+const http=require('http');
+const fs=require('fs');
+const minimist=require('minimist');
+
+const args = minimist(process.argv.slice(2));
+
+const server=http.createServer((req,resp)=>{
+    if(req.url==="/"){       
+        fs.readFile('home.html',(err,data)=>{
+            if(err){
+                resp.writeHead(404,{'Content-Type':'text/plain'});
+            }else{
+                resp.writeHead(200,{'Content-Type':'text/html'});
+                resp.end(data);
+            }           
+        })
+    }else if(req.url==='/registration'){
+        fs.readFile("registration.html",(err,data)=>{
+            if(err){
+                resp.writeHead(404,{'Content-Type':'text/plain'});
+            }else{
+                resp.writeHead(200,{'Content-Type':'text/html'});
+                resp.end(data);
+            }
+        })
+    }else if(req.url==='/projects'){
+        fs.readFile("project.html",(err,data)=>{
+            if(err){
+                resp.writeHead(404,{'Content-Type':'text/plain'});
+            }else{
+                resp.writeHead(200,{'Content-Type':'text/html'});
+                resp.end(data);
+            }
+        })
+    }else{
+        resp.writeHead(404,{'Content-Type':'text/plain'});
+    }
+}).listen(args.port)
